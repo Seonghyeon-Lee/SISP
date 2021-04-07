9fd0853ee43307a57c3e775728d3299586540b2f
Seonghyeon Lee <Seonghyeon.Lee@cognex.com>
2021-04-07 18:06:20 +0900

    * 기본 UI 업데이트

--- SISP/ViewModel/MainViewModel.cs
+++ SISP/ViewModel/MainViewModel.cs
@@ -1,34 +1,70 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace SISP.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        #region UI Variable
+        private int _FileManagerHeight;
+        public int FileManagerHeight
+        {
+            get => _FileManagerHeight;
+            set => Set(ref _FileManagerHeight, value);
+        }
+
+        private bool _IsOpenFileManager;
+        public bool IsOpenFileManager
+        {
+            get => _IsOpenFileManager;
+            set => Set(ref _IsOpenFileManager, value);
+        }
+
+        #endregion
+
+        #region Command
+        public RelayCommand<object> ButtonClickCommand { get; private set; }
+        public RelayCommand<System.Windows.SizeChangedEventArgs> WindowSizeChangedCommand { get; private set; }
+
+        private void InitRelayCommand()
+        {
+            ButtonClickCommand = new RelayCommand<object>(OnMenuClick);
+            WindowSizeChangedCommand = new RelayCommand<System.Windows.SizeChangedEventArgs>(OnWindowSizeChanged);
+        }
+
+        #region Command Action
+        private void OnMenuClick(object param)
+        {
+            switch (param.ToString())
+            {
+                case "OpenFileManager":
+                    OpenFileManager();
+                    break;
+                case "Exit":
+
+                    break;
+                case "EdgeDetection":
+                    ;
+                    break;
+            }
+        }
+
+        private void OpenFileManager()
+        {
+            IsOpenFileManager = true;
+        }
+
+        private void OnWindowSizeChanged(System.Windows.SizeChangedEventArgs e)
+        {
+            FileManagerHeight = (int)(e.NewSize.Height * 0.6);
+        }
+        #endregion
+        #endregion
+
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            InitRelayCommand();
+            FileManagerHeight = 400;
         }
     }
 }
